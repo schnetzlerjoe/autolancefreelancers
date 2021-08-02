e10d2cd9ad61de5317957279ee63ef8ecfdc5362
Joe Schnetzler <41382129+schnetzlerjoe@users.noreply.github.com>
2021-08-02 23:38:34 +0000

    Fix: Env config

--- src/lib/stores/config.js
+++ src/lib/stores/config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET
   };
